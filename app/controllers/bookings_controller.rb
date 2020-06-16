1df41295887998637457549ade3387b36c5556e5
anne constance pund <ac.pund@gmail.com>
2020-06-16 15:34:20 +0200

    work in progress

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,29 +1,29 @@
-# class BookingsController < ApplicationController
+class BookingsController < ApplicationController
 
-#     def new
-#          @booking = Booking.new
-#     end
+    def new
+         @booking = Booking.new
+    end
 
-#     def create
-#         @booking = Booking.new(booking_params)
-#         @booking.offer = Offer.find(params[offer_id])
-#         @booking.user = current_user
-#         if @booking.save
-#             redirect_to booking_path(@booking)
-#         else
-#             render :new
-#         end
-#     end
+    def create
+        @booking = Booking.new(booking_params)
+        @booking.offer = Offer.find(params[offer_id])
+        @booking.user = current_user
+        if @booking.save
+            redirect_to booking_path(@booking)
+        else
+            render :new
+        end
+    end
 
-#     def show
-#         @booking = Booking.find(params[:id])
-#         @review = Review.new
-#     end
+    def show
+        @booking = Booking.find(params[:id])
+        @review = Review.new
+    end
     
-# private
+private
 
-#   def booking_params
-#     params.require(:booking).permit(:user_id, :offer_id, :date)
-#   end
+  def booking_params
+    params.require(:booking).permit(:user_id, :offer_id, :date)
+  end
 
-# end
+end
