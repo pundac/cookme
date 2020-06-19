672b92ab0f3c572e13eeb926997441b2e46eaa65
anne constance pund <ac.pund@gmail.com>
2020-06-19 15:23:06 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,17 +35,23 @@ Offer.destroy_all
 Booking.destroy_all
 user_1 = User.new(email: "v.gmail", name: "Vincent")
 offer_1 = Offer.new(title: "Carbonara", description:"Spagetti,Creme", price: 20, user_id: 1, category_id: 1, address: "19 rue Inkermann, 59000 LILLE")
-booking_1 = Booking.new(date: , user_id: 2, offer_id:1)
+booking_1 = Booking.new(date: Fri, 03 Jul 2020 14:59:00 UTC +00:00, user_id: 2, offer_id:1)
 
 user_2 = User.new(email: "l.gmail", name: "Victor")
 
 
 user_3 = User.new(email: "ac.gmail", name: "Anne")
 offer_3 = Offer.new(title: "Boeuf Bourgignon", description: "Boeuf, beurre", price: 40, user_id: 3, category_id: 2, address: "23 rue Inkermann, 59000 LILLE")
-booking_3 = Booking.new(date: , user_id: 4, offer_id:3)
+booking_3 = Booking.new(date: Sat, 30 Jun 2020 17:00:00 UTC +00:00, user_id: 4, offer_id:3)
 
 user_4 = User.new(email: "Gasp.gmail", name: "Fou Fou")
 offer_4 = Offer.new(title: "Pas de Sushi", description: "mieux que au camping des flots bleu ", price: 1, user_id: 4, address: "2 Avenue des Saules, 59160 LILLE ")
+offer_
 
+carbonara = URI.open('https://images.pexels.com/photos/998246/pexels-photo-998246.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500')
+boeuf_bourgignon = URI.open('https://www.istockphoto.com/photo/boeuf-bourguignon-french-dish-gm1129663752-298499301?utm_campaign=srp_photos_noresults&utm_content=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fboeuf%2520bourgignon%2520%2F&utm_medium=affiliate&utm_source=pexels&utm_term=boeuf%20bourgignon')
+sushi = URI.open('https://images.pexels.com/photos/357756/pexels-photo-357756.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500')
 
-
+offer_1.photo.attach(io: carbonara, filename: 'carbonara.png', content_type: 'image/png' )
+offer_3.photo.attach(io: boeuf_bourgignon, filename: 'boeuf_bourgignon.png', content_type: 'image/png')
+offer_4.photo.attach(io: sushi, filename: 'sushi.png', content_type: 'image/png' )
